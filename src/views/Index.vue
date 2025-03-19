d225183888bb3d692f9350f60075ec9368190c31
Jairo Andrés Delgado López <tic@asservi.com>
2025-03-19 17:02:10 -0500

    first commit

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -33,7 +33,7 @@
                         <a href="#" class="-m-1.5 p-1.5">
                             <span class="sr-only">Enginer Jairo Delgado</span>
                             <img class="h-8 w-auto"
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                                src="@/assets/logojairodelgado.png"
                                 alt="" />
                         </a>
                         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700"
