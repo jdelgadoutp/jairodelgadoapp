b4f7bc076cc7e74f4c4f2f12c744d2c1bfb756aa
Jairo Andrés Delgado López <tic@asservi.com>
2025-03-19 23:19:57 -0500

    Updates

---
+++ assets/Contact-DLI5HRNw.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,a,o as s}from"./index-BZjlWT-z.js";const c={},r={class:"mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"};function n(l,o){return s(),t("div",r,o[0]||(o[0]=[a("h1",null,"Formulario de contacto Jairo Delgado",-1)]))}const d=e(c,[["render",n]]);export{d as default};
