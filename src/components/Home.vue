1213f0431515f8bca48db276c0d640f6c4cce08c
Jairo Andrés Delgado López <tic@asservi.com>
2025-03-20 17:24:59 -0500

    Links iniciio .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,30 +4,31 @@
             <div
                 class="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Es el presente. Innovar, automatizar y evolucionar
-                para liderar la era digital. <a href="#" class="font-semibold text-indigo-600"><span
+                para liderar la era digital. <Router-link :to="'/contact'" class="font-semibold text-indigo-600"><span
                         class="absolute inset-0" aria-hidden="true" />Más Información <span
-                        aria-hidden="true">&rarr;</span></a>
+                        aria-hidden="true">&rarr;</span></Router-link>
             </div>
         </div>
         <div class="text-center">
-            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Ing. Jairo
-                A. Delgado L.</h1>
-            <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Desarrollo |
-                Infraestructura | Seguridad IT </p>
+            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Jairo
+                Andrés Delgado López.</h1>
+            <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Ingeriero de Sistemas y Computación</p>
             <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#"
+                <Router-link :to="'/services'"
                     class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Ver
-                    Portafolio</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Más Información <span
-                        aria-hidden="true">→</span></a>
+                    Portafolio</Router-link>
+                <RouterLink :to="'/contact'" class="text-sm/6 font-semibold text-gray-900">Más Información
+                    <span aria-hidden="true">→</span>
+                </RouterLink>
             </div>
         </div>
     </div>
-    
+
 </template>
 
 <script>
-import Footers from '@/views/Footers.vue';
+import { RouterLink } from 'vue-router';
+
 
 
 
--- src/views/Footers.vue
+++ src/views/Footers.vue
@@ -2,11 +2,11 @@
     <footer class="text-gray-700 py-6">
         <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-3">
             <div class="flex lg:flex-1">
-                    <a href="#" class="-m-1.5 p-1.5">
-                        <span class="sr-only">Enginer Jairo Delgado</span>
-                        <img class="h-8 w-auto" src="@/assets/logojairodelgado.png" alt="" />
-                    </a>
-                </div>
+                <a href="#" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Enginer Jairo Delgado</span>
+                    <img class="h-8 w-auto" src="@/assets/logojairodelgado.png" alt="" />
+                </a>
+            </div>
             <!-- Contenedor principal -->
             <div class="flex flex-wrap justify-center space-x-6 md:space-x-10 text-sm">
                 <a href="#" class="hover:text-white">About</a>
@@ -24,7 +24,7 @@
                 <a href="#" class="hover:text-white">GitHub</a>
                 <a href="#" class="hover:text-white">YouTube</a>
             </div>
-            
+
             <!-- Línea divisoria -->
             <div class="border-t border-gray-700 mt-4 pt-4 text-center">
                 <p class="text-sm">&copy; 2025 Jairo Andrés Delgado López, Inc. All rights reserved.</p>
