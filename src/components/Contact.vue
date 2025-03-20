773bc70c4b003f25728e11468760d05adc3cc8d7
Jairo Andrés Delgado López <tic@asservi.com>
2025-03-19 23:35:28 -0500

    Formulario de contacto.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,99 @@
 <template>
     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <h1>Formulario de contacto Jairo Delgado</h1>
+        <div class="mx-auto max-w-2xl text-center">
+            <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+                Contacto</h2>
+            <p class="mt-2 text-lg/8 text-gray-600">Envíanos tu consulta y nuestro equipo te responderá a la brevedad.
+            </p>
+        </div>
+        <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                    <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">Nombres</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    </div>
+                </div>
+                <div>
+                    <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Apellidos</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="company" class="block text-sm/6 font-semibold text-gray-900">Compañia</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="company" id="company" autocomplete="organization"
+                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
+                    <div class="mt-2.5">
+                        <input type="email" name="email" id="email" autocomplete="email"
+                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Teléfono Movil</label>
+                    <div class="mt-2.5">
+                        <div
+                            class="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                            <div class="grid shrink-0 grid-cols-1 focus-within:relative">
+                                <select id="country" name="country" autocomplete="country" aria-label="Country"
+                                    class="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                    <option>CO</option>
+                                    <option>MX</option>
+                                    <option>EU</option>
+                                </select>
+                                <ChevronDownIcon
+                                    class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                                    aria-hidden="true" />
+                            </div>
+                            <input type="text" name="phone-number" id="phone-number"
+                                class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                placeholder="123-456-7890" />
+                        </div>
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="message" class="block text-sm/6 font-semibold text-gray-900">Mensaje</label>
+                    <div class="mt-2.5">
+                        <textarea name="message" id="message" rows="4"
+                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    </div>
+                </div>
+                <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
+                    <div class="flex h-6 items-center">
+                        <Switch v-model="agreed"
+                            :class="[agreed ? 'bg-indigo-600' : 'bg-gray-200', 'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-gray-900/5 transition-colors duration-200 ease-in-out ring-inset focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']">
+                            <span class="sr-only">Aceptar políticas</span>
+                            <span aria-hidden="true"
+                                :class="[agreed ? 'translate-x-3.5' : 'translate-x-0', 'size-4 transform rounded-full bg-white ring-1 shadow-xs ring-gray-900/5 transition duration-200 ease-in-out']" />
+                        </Switch>
+                    </div>
+                    <SwitchLabel class="text-sm/6 text-gray-600">
+                        Al seleccionar esto, usted acepta nuestra 
+                        {{ ' ' }}
+                        <a href="#" class="font-semibold text-indigo-600">política&nbsp;de privacidad</a>.
+                    </SwitchLabel>
+                </SwitchGroup>
+            </div>
+            <div class="mt-10">
+                <button type="submit"
+                    class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Hablemos</button>
+            </div>
+        </form>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { ChevronDownIcon } from '@heroicons/vue/16/solid'
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+
+const agreed = ref(false)
+</script>
